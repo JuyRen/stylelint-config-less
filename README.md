65e6f2eec640e3f2edfb9187858fa7e08e7cfc4e
juyren <juyren@163.com>
2022-07-15 16:31:18 +0800

    feat: 添加readme.md 和 example文件夹

---
+++ README.md
@@ -0,0 +1,18 @@
+# Stylelint Less 配置集合
+
+本项目，将所有依赖都安装在`dependencies`中，做一个集合操作。
+在正式开发的项目中，无需再次安装其他依赖，只需要安装该仓库即可
+
+## 安装
+
+```shell
+  yarn add @juyren/stylelint-config-personal -D
+```
+
+## 集成功能
+
+1. 使用`stylelint-config-standard`规则
+2. 自动排序 `stylelint-config-recess-order`
+3. 忽略与 prettier 冲突的规则 `stylelint-config-prettier`
+4. 检查可能存在冲突的属性值 `stylelint-declaration-block-no-ignored-properties`
+5. 预设`rules`
--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
-  plugins: ["stylelint-declaration-block-no-ignored-properties"],
-  customSyntax: "postcss-less", // less语法
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier',
+  ],
+  /**
+   * 1. stylelint-declaration-block-no-ignored-properties (https://github.com/kristerkari/stylelint-declaration-block-no-ignored-properties)
+   */
+  plugins: ['stylelint-declaration-block-no-ignored-properties'],
   rules: {
+    // 解决在元素选择器内 @import 文件时，校验问题
+    'no-invalid-position-at-import-rule': null,
+
     /**
-     * 配合stylelint-declaration-block-no-ignored-properties插件
-     * https://github.com/kristerkari/stylelint-declaration-block-no-ignored-properties
-     * */
-    "plugin/declaration-block-no-ignored-properties": true,
-    "string-quotes": "single",
-    "no-invalid-position-at-import-rule": null,
-    "value-no-vendor-prefix": null, // https://stylelint.io/user-guide/rules/list/value-no-vendor-prefix/
+     * https://stylelint.io/user-guide/rules/list/value-no-vendor-prefix/
+     * 可以解决 display: -webkit-box， 校验问题
+     */
+    'value-no-vendor-prefix': null,
+
+    'font-family-no-missing-generic-family-keyword': [
+      true,
+      {
+        ignoreFontFamilies: [
+          'PingFangSC-Light',
+          'PingFangSC-Medium',
+          'PingFangSC-Semibold',
+        ],
+      },
+    ],
+
+    // 配合stylelint-declaration-block-no-ignored-properties插件
+    'plugin/declaration-block-no-ignored-properties': true,
   },
+
+  // https://stylelint.io/user-guide/usage/options/#customsyntax
+  customSyntax: 'postcss-less',
+  /**
+   * If you want to lint two or more different languages, you can combine customSyntax with the
+   * overrides configuration property ===> https://stylelint.io/user-guide/configure/#overrides.
+   */
+
+  // overrides: [
+  //   {
+  //     files: ['**/*.scss'],
+  //     customSyntax: 'postcss-scss',
+  //   },
+  //   {
+  //     files: ['**/*.less'],
+  //     customSyntax: 'postcss-less',
+  //   },
+  // ],
 };
